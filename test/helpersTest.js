d431200f7a839cadfcfc8a3a4973cddbf7c502eb
murray-pocha <murray.pocha@gmail.com>
2024-11-26 17:39:45 -0700

    built tests through AI assist to test function.

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -1,6 +1,6 @@
 const { assert } = require('chai');
 
-const { getUserByEmail } = require('../helpers.js');
+const { getUserByEmail, urlsForUser } = require('../helpers.js');
 
 const testUsers = {
   "userRandomID": {
@@ -14,6 +14,68 @@ const testUsers = {
     password: "dishwasher-funk"
   }
 };
+
+describe('urlsForUser', function() {
+  const urlDatabase = {
+    "b2xVn2": { longURL: "http://www.lighthouselabs.ca", userID: "userRandomID" },
+    "9sm5xK": { longURL: "http://www.google.com", userID: "user2RandomID" },
+    "b6y3Tc": { longURL: "http://www.example.com", userID: "userRandomID" }
+  };
+
+  it('should return only the URLs belonging to the specified user', function() {
+    const userId = "userRandomID"; // Specify the user to check for
+    const expectedUrls = {
+      "b2xVn2": { longURL: "http://www.lighthouselabs.ca", userID: "userRandomID" },
+      "b6y3Tc": { longURL: "http://www.example.com", userID: "userRandomID" }
+    };
+
+    const userUrls = urlsForUser(userId, urlDatabase);
+
+    // Assert that the returned value is an object
+    assert.isObject(userUrls, 'The returned value should be an object');
+
+    // Assert that the returned URLs belong to the specified user
+    assert.deepEqual(userUrls, expectedUrls, 'The returned URLs should match the expected URLs for the user');
+  });
+
+  it('should return an empty object if the urlDatabase does not contain any URLs that belong to the specified user', function() {
+    const userId = "nonExistentUser"; // User ID that doesn't exist in the urlDatabase
+    const userUrls = urlsForUser(userId, urlDatabase);
+
+    // Assert that the returned value is an empty object
+    assert.deepEqual(userUrls, {}, 'The returned value should be an empty object for a user with no URLs');
+  });
+
+  it('should return an empty object if the urlDatabase is empty', function() {
+    const userId = "userRandomID"; // Specify the user to check for
+    const emptyUrlDatabase = {}; // Empty urlDatabase
+
+    const userUrls = urlsForUser(userId, emptyUrlDatabase);
+
+    // Assert that the returned value is an empty object
+    assert.deepEqual(userUrls, {}, 'The returned value should be an empty object when the urlDatabase is empty');
+  });
+
+  it('should not return any URLs that do not belong to the specified user', function() {
+    const userId = "userRandomID"; // Specify the user to check for
+    const expectedUrls = {
+      "b2xVn2": { longURL: "http://www.lighthouselabs.ca", userID: "userRandomID" },
+      "b6y3Tc": { longURL: "http://www.example.com", userID: "userRandomID" }
+    };
+
+    const userUrls = urlsForUser(userId, urlDatabase);
+
+    // Assert that the returned value is an object
+    assert.isObject(userUrls, 'The returned value should be an object');
+
+    // Assert that the returned URLs are correct and do not contain URLs belonging to other users
+    assert.deepEqual(userUrls, expectedUrls, 'The returned URLs should match the expected URLs for the user');
+    
+    // Assert that the returned object does not contain URLs that do not belong to the user
+    assert.isUndefined(userUrls["9sm5xK"], 'The returned URLs should not include URLs that do not belong to the specified user');
+  });
+});
+
 
 describe('getUserByEmail', function() {
   it('should return a user with valid email', function() {
