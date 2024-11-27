2913f73183550bb3474aee8104aa436f328d3f32
murray-pocha <murray.pocha@gmail.com>
2024-11-26 17:38:33 -0700

    add urlsforuser function and add to exports to better modularize

--- helpers.js
+++ helpers.js
@@ -10,4 +10,23 @@ const getUserByEmail = function(email, database) {
   return null; //if no user was found return null or undefined
 };
 
-module.exports = { getUserByEmail };
+
+
+
+// function to filter URLs based on the user ID
+const urlsForUser = (userId, urlDatabase) => {
+  const userUrls = {}; //initialize an object to store user-specific URLs
+
+  // loop through the entire urlDatabase
+  for (let shortUrl in urlDatabase) {
+
+    // if the userID of the URL matches the logged-in users ID, add it to userUrls
+    if (urlDatabase[shortUrl].userID === userId) {
+      userUrls[shortUrl] = urlDatabase[shortUrl];
+    }
+  }
+
+  return userUrls; // return the filtered URLs/ or empty object
+};
+
+module.exports = { getUserByEmail, urlsForUser };
